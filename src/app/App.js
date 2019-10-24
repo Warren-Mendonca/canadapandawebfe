37f2c4f68cf3a47be1033b69ab1119e9512f0bc1
Warren Mendonca <warren.mendonca@gmail.com>
2019-10-23 23:01:58 -0400

    Canada panda fe first commit

---
+++ src/app/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+  Route,
+  withRouter,
+  Switch
+} from 'react-router-dom';
+
+import { getCurrentUser } from '../utils/APIUtils';
+import { ACCESS_TOKEN, APP_NAME } from '../constants';
+
+import Login from '../user/login/Login';
+import Signup from '../user/signup/Signup';
+import Profile from '../user/profile/Profile';
+import AppHeader from '../common/AppHeader';
+import NotFound from '../common/NotFound';
+import LoadingIndicator from '../common/LoadingIndicator';
+
+import { Layout, notification } from 'antd';
+const { Content } = Layout;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      isAuthenticated: false,
+      isLoading: false
+    }
+    this.handleLogout = this.handleLogout.bind(this);
+    this.loadCurrentUser = this.loadCurrentUser.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
+    notification.config({
+      placement: 'bottomRight',
+      top: 70,
+      duration: 3,
+    });
+  }
+
+  loadCurrentUser() {
+    this.setState({
+      isLoading: true
+    });
+    getCurrentUser()
+        .then(response => {
+          this.setState({
+            currentUser: response,
+            isAuthenticated: true,
+            isLoading: false
+          });
+        }).catch(error => {
+      this.setState({
+        isLoading: false
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.loadCurrentUser();
+  }
+
+  handleLogout(redirectTo="/", notificationType="success", description="You're successfully logged out.") {
+    localStorage.removeItem(ACCESS_TOKEN);
+
+    this.setState({
+      currentUser: null,
+      isAuthenticated: false
+    });
+
+    this.props.history.push(redirectTo);
+
+    notification[notificationType]({
+      message: APP_NAME,
+      description: description,
+    });
+  }
+
+  handleLogin() {
+    notification.success({
+      message: APP_NAME,
+      description: "You're successfully logged in.",
+    });
+    this.loadCurrentUser();
+    this.props.history.push("/");
+  }
+
+  render() {
+    if(this.state.isLoading) {
+      return <LoadingIndicator />
+    }
+    return (
+        <Layout className="app-container">
+          <AppHeader isAuthenticated={this.state.isAuthenticated}
+                     currentUser={this.state.currentUser}
+                     onLogout={this.handleLogout} />
+
+          <Content className="app-content">
+            <div className="container">
+              <Switch>
+                <Route path="/login"
+                       render={(props) => <Login onLogin={this.handleLogin} {...props} />}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/users/:username"
+                       render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}>
+                </Route>
+                <Route component={NotFound}/>
+              </Switch>
+            </div>
+          </Content>
+        </Layout>
+    );
+  }
+}
+
+export default withRouter(App);
---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const APP_NAME = 'Canada Panda App';
+
+export const API_BASE_URL = 'http://localhost:8080';
+export const ACCESS_TOKEN = 'accessToken';
+
+export const NAME_MIN_LENGTH = 3;
+export const NAME_MAX_LENGTH = 20;
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 40;
+
+export const EMAIL_MAX_LENGTH = 40;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 20;
